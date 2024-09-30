d378913ddc082a249fd3eabab916cab59dc6a18b
CarlosEnoc Backend <enoc.calzada1@gmail.com>
2024-09-29 21:28:25 -0600

    Se creo la Clase Control para recibir los parametro por URL

--- app/libs/Control.php
+++ app/libs/Control.php
@@ -1,13 +1,33 @@
 <?php
+/*
+primer parametro : Controlador
+segundo parametro:el metodo
 
+*/
 
 class Control {
 
-function __construct(){
+    function __construct()
+	{
+		$url = $this->separarURL();
+		var_dump($url);
+	}
 
-print("Binevenidio a la APP");
+	public function separarURL()
+	{
+		$url = "";
+		if (isset($_GET['url'])) {
+			// eliminamos el caracter final
+			$url = rtrim($_GET['url'],"/");
+			$url = rtrim($_GET['url'],"\\");
+			//Sanitizamos
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			//
+			$url = explode("/",$url);
+		}
+		return $url;
+	}
 
-}
 
 }
 
