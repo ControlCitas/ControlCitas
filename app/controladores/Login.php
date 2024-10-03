23f21d45bff5e9192790bddcea20a1cc8afeef84
carlos enoc Backend <enoc.calzada1@gmail.com>
2024-10-03 17:54:09 -0600

    Se anadio validacion del correro como manejo de errores del Controlador

--- app/controladores/Login.php
+++ app/controladores/Login.php
@@ -22,13 +22,43 @@ class Login extends Controlador
 
 	public function olvido()
 	{
-		$datos = [
+		$errores = [];
+		if ($_SERVER['REQUEST_METHOD']=="POST") {
+			//Recepción de los datos
+			$email = $_POST["correo"]??"";
+			//Validación
+			if ($email=="") {
+				array_push($errores,"El correo electrónico es requerido");
+			}
+			if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+				array_push($errores,"El correo electrónico no es válido");
+			}
+			//Proceso
+			if (empty($errores)) {
+				if ($this->modelo->validarCorreo($email)) {
+					var_dump("Si se encontró el correo");
+					exit(0);
+				} else {
+					array_push($errores,"El correo electrónico no se encuentra en nuestra base de datos.");
+				}
+				
+			} else {
+				$datos = [
+				"titulo" => "Olvido de contraseña",
+				"subtitulo" => "¿Olvidaste tu contraseña?",
+				"errores" => $errores,
+				"datos" => []
+				];
+				$this->vista("loginOlvidoVista",$datos);
+			}
+		} else {
+			$datos = [
 			"titulo" => "Olvido de contraseña",
 			"subtitulo" => "¿Olvidaste tu contraseña?",
+			"errores" => $errores,
 			"datos" => []
-		];
-		$this->vista("loginOlvidoVista",$datos);
+			];
+			$this->vista("loginOlvidoVista",$datos);
+		}
 	}
 }
-
-
--- app/modelos/LoginModelo.php
+++ app/modelos/LoginModelo.php
@@ -2,15 +2,19 @@
 /**
  * Login Modelo
  */
+class LoginModelo
+{
+	public $db;
+		
+	function __construct()
+	{
+		$this->db = new MySQLdb();
+	}
 
-class LoginModelo {
-
-
-    function __construct(){
-        $this->db= new MySQLdb();
-    }
-
-
+	public function validarCorreo($email)
+	{
+		$sql ="SELECT * FROM admon WHERE correo='".$email."'";
+		$data = $this->db->query($sql);
+		return (count($data)==0)?false:true;
+	}
 }
-
-?>
