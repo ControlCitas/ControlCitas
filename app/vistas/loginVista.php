3ca06a4023a54a84a40c6dfb355df5d108acfde2
Rene Suriano <renefront@gmail.com>
2024-10-03 11:59:23 -0600

    se instalo boostrap para la vista principal del sistema

--- app/vistas/loginVista.php
+++ app/vistas/loginVista.php
@@ -4,21 +4,36 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Consultorio médico</title>
+	<link rel="shortcut icon" href="../public/ing/favicon.ico">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </head>
 <body>
-	<table>
-		<tr>
-			<td>Usuario:</td>
-			<td><input type="text" name="usuario"></td>
-		</tr>
-		<tr>
-			<td>Clave de acceso:</td>
-			<td><input type="password" name="clave"></td>
-		</tr>
-		<tr>
-			<td>&nbsp;</td>
-			<td><input type="submit" value="Enviar"></td>
-		</tr>
+	<nav class="navbar navbat-espand-sm bg-dark navbar-dark">
+		<a href="" class navbar-brand>Consultorio</a>
+	</nav>
+	<div class="container-fluid">
+		<div class="row content">
+			<div class="col-sm-2"></div>
+			<div class="col-sm-8">
+				<table>
+			<tr>
+				<td>Usuario:</td>
+				<td><input type="text" name="usuario"></td>
+			</tr>
+			<tr>
+				<td>Clave de acceso:</td>
+				<td><input type="password" name="clave"></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td><input type="submit" value="Enviar"></td>
+			</tr>
 	</table>
+
+			</div>
+			<div class="col-sm-2"></div>
+		</div>
+	</div>
 </body>
 </html>
